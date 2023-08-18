47428a8affe3a755742d36761a2795854a108fb7
Over-lord12 <uchimezie12@gmail.com>
2023-08-18 11:14:03 -0700

    completed

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,12 +3,12 @@
 /**
  * _isupper - checks for uppercase
  * @c: the character
- * Return: 1 if c is uppercase 
+ * Return: 1 if c is uppercase
  * otherwise 0
  */
 int _isupper(int c)
 {
-	if (c >= 65 && c >=  90)
+	if (c >= 'A' && c >=  'Z')
 		return (1);
 	else
 		return (0);
