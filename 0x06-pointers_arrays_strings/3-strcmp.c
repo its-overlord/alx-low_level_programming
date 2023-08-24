193fa3b39f8458ade85c1c9723c3c453a5bbf170
Over-lord12 <uchimezie12@gmail.com>
2023-08-23 18:45:53 -0700

    Completed

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,16 +10,12 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 != '\0')
+	while (*s1 == *s2)
 	{
-		if (*s1 == *s2)
+		if (*s1 != '\0')
 			return (0);
-		else
-		{
-			return(*s1 - *s2);
-			break;
-		}
 		s1++;
 		s2++;
 	}
+	return(*s1 - *s2);
 }
