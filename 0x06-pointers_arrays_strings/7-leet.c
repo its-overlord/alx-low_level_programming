df397a68c8370c66795be703678977a99eb7429a
Over-lord12 <uchimezie12@gmail.com>
2023-08-23 20:01:59 -0700

    Completed

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * leet - encodes string into 1337
+ * @s: pointer to the sting to be encoded
+ * Return: the encoded string
+ */
+char *leet(char *s)
+{
+	char *a = "aAeEoOtTlL";
+	char *b = "4433007711";
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == a[j])
+				s[i] = b[j];
+		}
+	}
+	return (s);
+}
