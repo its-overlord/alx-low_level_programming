530401c8e18159a6fcfbb42c5dc4c6636cb1193b
Over-lord12 <uchimezie12@gmail.com>
2023-08-28 17:53:09 -0700

    Completed

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *_strstr - searches a string for any of a set of bytes
+ * @haystack: pointer to string to be sesrched
+ * @needle: pointer to the string that
+ * represents the substring you're looking for
+ * Return: a pointer to the first occurrence of any
+ * character from the needle set within haystack
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i] == needle[j])
+				return (needle);
+		}
+	}
+	return (NULL);
+}
