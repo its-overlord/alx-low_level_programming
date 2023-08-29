82980d4e26c5ebcfeb5b3fea58c2c02f24b4efcc
Over-lord12 <uchimezie12@gmail.com>
2023-08-28 17:38:13 -0700

    Completed

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,15 +10,16 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	int i, j;
+	int j;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while  (*s != '\0')
 	{
-		for (j = 0; accept[i] != '\0'; j++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (s[i] == accept[j])
-				return (s + i);
+			if (*s == accept[j])
+				return (s);
 		}
+		s++;
 	}
 	return (NULL);
 }
