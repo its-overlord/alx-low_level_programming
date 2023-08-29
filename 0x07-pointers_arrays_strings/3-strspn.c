f83a2ed0a2f9e70a79438b5535bab04f5efecff3
Over-lord12 <uchimezie12@gmail.com>
2023-08-28 17:15:04 -0700

    Completed

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,11 +12,13 @@ unsigned int _strspn(char *s, char *accept)
 {
 	int i, j, count, flag;
 
-	for (i = 0; i != '\0'; i++)
+	count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		flag = 0;
 
-		for (j = 0; j != '\0'; j++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
