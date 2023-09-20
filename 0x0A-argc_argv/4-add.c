2bfeb497b79dbf5586135269a831ce80499b6814
Over-lord12 <uchimezie12@gmail.com>
2023-09-20 10:31:30 -0700

    Completed

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,6 +2,42 @@
 #include <stdlib.h>
 
 /**
+ * _atoi - converts a string to an integer
+ * @s: the string
+ * Return: the int converted from the sting
+ */
+int _atoi(char *s)
+{
+	int i, sign, num, len, digit;
+
+	i = 0;
+	sign = 1;
+	num = 0;
+	len = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len)
+	{
+		if (s[0] == '-')
+		{
+			sign = -1;
+			i++;
+		}
+
+		if (s[i] >= '0' && s[i] <='9')
+		{
+			digit = s[i] - '0';
+			num = num * 10 + digit;
+		}
+		i++;
+	}
+	return (num * sign);
+}
+
+/**
  * main - multiples two numbers
  * @argc: number of command line arguements
  * @argv: an array of command line arguements
@@ -9,19 +45,28 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, sum;
+	int sum, i, j, k;
 
 	sum = 0;
+	i = 1;
 
-	for (i = 1; i < argc; i++)
+	while (i < argc)
 	{
-		if (atoi(argv[i]))
-			sum += atoi(argv[i]);
-		else
+		j = 0;
+		while (argv[i][j] != '\0')
 		{
-			printf("Error\n");
-			return (1);
+			if (!(argv[i][j] < '9'&& argv[i][j] > '0'))
+			{
+				puts("Error");
+				return (1);
+			}
+			j++;
 		}
+		i++;
+	}
+	for (k = 1; k < argc; k++)
+	{
+		sum += _atoi(argv[k]);
 	}
 	printf("%d\n", sum);
 	return (0);
