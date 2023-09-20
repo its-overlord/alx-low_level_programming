2f575757f312df525248c882518159a4edc0e24b
Over-lord12 <uchimezie12@gmail.com>
2023-09-20 11:45:17 -0700

    Completed

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -5,13 +5,14 @@
  * main -  prints the minimum number of coins to
  * make change for an amount of money.
  * @argc: the number of command line arguments
- * @argv[]: an array of command line arguements
+ * @argv: an array of command line arguements
  * Return: alway 0 at success
  */
 int main(int argc, char *argv[])
 {
 	int deno[] = {25, 10, 5, 2, 1};
 	int i, j, num;
+
 	j = 0;
 
 	if (argc != 2)
