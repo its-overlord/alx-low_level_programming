88d53b562b4c9dd2bca8ef8f38d9f3e556ff4500
Over-lord12 <uchimezie12@gmail.com>
2023-09-27 10:01:40 -0700

    Completed

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,47 +1,49 @@
+#include <stdlib.h>
 #include "main.h"
 
 /**
- * str_concat - concatenates two strings.
- * @s1: first string
- * @s2: second string
+ * *str_concat - concatenates two strings
+ * @s1: string to concatenate
+ * @s2: other string to concatenate
  *
- * Return: pointer to a duplicate of the string str
+ * Return: pointer to the new string created (Success), or NULL (Error)
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *ptr;
-	int i, j, k;
+	char *s3;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	for (i = 0; s1[i] != '\0'; i++)
-		;
-	j = 0;
-	while (s2[j] != '\0')
-	{
-		j++;
-		i++;
-	}
-	ptr = malloc((i + 1) * sizeof(char));
-	if (ptr == NULL)
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
+
+	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (s3 == NULL)
 		return (NULL);
+
+	i = 0;
 	j = 0;
-	k = 0;
+
 	if (s1)
 	{
-		while (s1[j] != '\0')
+		while (i < len1)
 		{
-			ptr[j] = s1[j];
-			j++;
+			s3[i] = s1[i];
+			i++;
 		}
 	}
+
 	if (s2)
 	{
-		while (s2[k] != '\0')
+		while (i < (len1 + len2))
 		{
-			ptr[j] = s2[k];
-			k++;
+			s3[i] = s2[j];
+			i++;
 			j++;
 		}
 	}
-	ptr[j] = '\0';
-	return (ptr);
+	s3[i] = '\0';
+
+	return (s3);
 }
