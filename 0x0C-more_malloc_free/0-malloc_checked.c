eb1024bdc9c21a5912e01e79faa2f25a72a5e5a6
Over-lord12 <uchimezie12@gmail.com>
2023-09-30 15:54:37 -0700

    Completed

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -7,12 +7,12 @@
  * Return: a pointer to the allocated memory
  */
 void *malloc_checked(unsigned int b)
-{ 
- 	void *str;
- 
- 	str = malloc(b);
- 
- 	if (str == NULL)
+{
+	void *str;
+
+	str = malloc(b);
+
+	if (str == NULL)
  		exit(98);
 
 	return (str);
