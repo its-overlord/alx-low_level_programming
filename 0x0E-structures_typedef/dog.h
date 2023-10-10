bc795edaac2843567730ef1a7b4cee1ec9bf8601
Over-lord12 <uchimezie12@gmail.com>
2023-10-09 21:38:27 -0700

    Completed

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - a dog info
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ */
+
 struct dog
 {
 	char *name;
@@ -8,5 +15,6 @@ struct dog
 	char *owner;
 };
 
+typedef struct dog dog_t;
 #include <stdio.h>
 #endif
