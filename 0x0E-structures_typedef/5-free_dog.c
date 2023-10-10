f64df49bfa674f47f384cfce338b0af328a47227
Over-lord12 <uchimezie12@gmail.com>
2023-10-10 00:03:18 -0700

    Completed

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+
+/**
+ * free_dog - a function that frees dogs
+ * @d: the structure to the dog to free
+ */
+void free_dog(dog_t *d)
+{
+	if !(d == NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
